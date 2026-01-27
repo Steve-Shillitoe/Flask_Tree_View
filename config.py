3de4bab00f41e1ff2a380875b1c6d08eedad1785
Steve Shillitoe <s.shillitoe99@gmail.com>
2026-01-27 18:32:28 +0000

    Improved comments

--- config.py
+++ config.py
@@ -1,4 +1,19 @@
-# config.py
+
+"""
+Application configuration module.
+
+This module defines configuration values for the Flask application,
+including database connection settings and SQLAlchemy behaviour.
+"""
+
+# SQLAlchemy database connection URI.
+#
+# This configuration uses:
+# - Microsoft SQL Server (SQLEXPRESS)
+# - Windows authentication (trusted connection)
+# - pyodbc as the database driver
+#
+# The database name is "AnimalKingdom".
 SQLALCHEMY_DATABASE_URI = (
     "mssql+pyodbc://@localhost\\SQLEXPRESS/AnimalKingdom"
     "?driver=ODBC+Driver+17+for+SQL+Server"
@@ -6,5 +21,8 @@ SQLALCHEMY_DATABASE_URI = (
     "&TrustServerCertificate=yes"
 )
 
+# Disable SQLAlchemy event system for object modification tracking.
+# This reduces memory overhead and is recommended unless explicitly needed.
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
